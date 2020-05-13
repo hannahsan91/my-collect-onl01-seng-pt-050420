d974285337b41c2a39225f1938acd29df1caaa79
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-13 01:44:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,3 @@
 
-def my_collect
-  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-    my_collect(array) do |name|
-      name.split(" ").first
-  end
-  collection = ["ruby", "javascript", "python", "objective-c"]
-  my_collect(collection) do |lang|
-    lang.upcase
-  end
+def my_collect(array)
 end
